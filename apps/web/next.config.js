433876d836fa8e1d0d9e55421383263bb55ce2ca
nadia24021986-ship-it <nadia24021986@gmail.com>
2026-07-23 20:22:24 +0700

    Create next.config.js

---
+++ apps/web/next.config.js
@@ -0,0 +1,23 @@
+const path = require('path');
+const withNextIntl = require('next-intl/plugin')('./i18n/request.ts');
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  outputFileTracingRoot: path.join(__dirname, '../../'),
+  experimental: {
+    serverActions: {
+      bodySizeLimit: '2mb',
+    },
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**.supabase.co',
+      },
+    ],
+  },
+};
+
+module.exports = withNextIntl(nextConfig);
